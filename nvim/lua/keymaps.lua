5b0c0714d558c499bcffd95d71c092d02e26fb90
nomyfan <Nomyfan@live.com>
2022-12-05 08:59:59 +0800

    update keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -8,27 +8,31 @@ function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-map("n", "<Space>", "<Nop>")
+function nmap(lhs, rhs, opts)
+  map('n', lhs, rhs, opts)
+end
+
+nmap("<Space>", "<Nop>", { noremap = false })
 
 -- Split window
-map("n", "sj", ":set splitbelow<CR>:split<CR>", { silent = true} )
-map("n", "sk", ":set nosplitbelow<CR>:split<CR>", { silent = true} )
-map("n", "sh", ":set nosplitright<CR>:vsplit<CR>", { silent = true} )
-map("n", "sl", ":set splitright<CR>:vsplit<CR>", { silent = true} )
+nmap("sj", ":set splitbelow<CR>:split<CR>", { silent = true} )
+nmap("sk", ":set nosplitbelow<CR>:split<CR>", { silent = true} )
+nmap("sh", ":set nosplitright<CR>:vsplit<CR>", { silent = true} )
+nmap("sl", ":set splitright<CR>:vsplit<CR>", { silent = true} )
 
 -- Move window
-map("n", "<leader><SPACE>", "<C-w>w")
-map("n", "<leader><left>", "<C-w>h")
-map("n", "<leader><down>", "<C-w>j")
-map("n", "<leader><up>", "<C-w>k")
-map("n", "<leader><right>", "<C-w>l")
-map("n", "<leader>h", "<C-w>h")
-map("n", "<leader>j", "<C-w>j")
-map("n", "<leader>k", "<C-w>k")
-map("n", "<leader>l", "<C-w>l")
+nmap("<leader><SPACE>", "<C-w>w")
+--nmap("<leader><left>", "<C-w>h")
+--nmap("<leader><down>", "<C-w>j")
+--nmap("<leader><up>", "<C-w>k")
+--nmap("<leader><right>", "<C-w>l")
+nmap("<leader>h", "<C-w>h")
+nmap("<leader>j", "<C-w>j")
+nmap("<leader>k", "<C-w>k")
+nmap("<leader>l", "<C-w>l")
 
 -- Resize window
-map("n", "<C-w><left>", ":vertical res -10<CR>")
-map("n", "<C-w><right>", ":vertical res +10<CR>")
-map("n", "<C-w><up>", ":res -10<CR>")
-map("n", "<C-w><down>", ":res -10<CR>")
+nmap("<leader><left>", ":vertical res -10<CR>")
+nmap("<leader><right>", ":vertical res +10<CR>")
+nmap("<leader><up>", ":res -10<CR>")
+nmap("<leader><down>", ":res -10<CR>")

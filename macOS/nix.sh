9ee8cf9d99939f6045a6fea9d1bdd92b9f9e0bee
nomyfan <Nomyfan@live.com>
2023-06-25 22:27:07 +0800

    .

--- macOS/nix.sh
+++ macOS/nix.sh
@@ -1,18 +1,21 @@
 #!/bin/zsh
 
 # Ensure nix has been installed
-nix-env -iA nixpkgs.openssl
-nix-env -iA nixpkgs.ripgrep
-nix-env -iA nixpkgs.fd
-nix-env -iA nixpkgs.htop
-nix-env -iA nixpkgs.neovim
-nix-env -iA nixpkgs.powershell
-nix-env -iA nixpkgs.tealdeer
-nix-env -iA nixpkgs.tokei
-nix-env -iA nixpkgs.zoxide
-nix-env -iA nixpkgs.jq
-nix-env -iA nixpkgs.gnupg
-nix-env -iA nixpkgs.delta
-nix-env -iA nixpkgs.jless
-nix-env -iA nixpkgs.tree
-
+nix-env -iA nixpkgs.openssl \
+  nixpkgs.ripgrep \
+  nixpkgs.fd \
+  nixpkgs.htop \
+  nixpkgs.neovim \
+  nixpkgs.powershell \
+  nixpkgs.tealdeer \
+  nixpkgs.tokei \
+  nixpkgs.zoxide \
+  nixpkgs.jq \
+  nixpkgs.gnupg \
+  nixpkgs.delta \
+  nixpkgs.jless \
+  nixpkgs.tree \
+  nixpkgs.du-dust \
+  nixpkgs.bat \
+  nixpkgs.rustup \
+  nixpkgs.exa

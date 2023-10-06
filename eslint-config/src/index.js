e16cfd17ce238a7ee302b181683bd729b703286e
nomyfan <Nomyfan@live.com>
2023-10-06 23:33:34 +0800

    Update eslint config

--- eslint-config/src/index.js
+++ eslint-config/src/index.js
@@ -1,6 +1,40 @@
 const configBasic = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+  ],
   parser: "@typescript-eslint/parser",
+  plugins: ["import"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+
+    "import/default": "error",
+    "import/namespace": "error",
+    "import/export": "error",
+    "import/first": "error",
+    "import/no-default-export": "error",
+    "import/consistent-type-specifier-style": ["warn", "prefer-top-level"],
+    "import/newline-after-import": "error",
+    "import/no-anonymous-default-export": "error",
+    "import/no-deprecated": "warn",
+    "import/no-empty-named-blocks": "error",
+    "import/no-extraneous-dependencies": "error",
+    "import/no-named-as-default": "error",
+    "import/no-self-import": "error",
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "parent", "sibling", "index"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          orderImportKind: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
 
 const configReact = {
@@ -8,7 +42,13 @@ const configReact = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:import/react",
   ],
+  settings: {
+    react: {
+      version: "18",
+    },
+  },
 };
 
 const configFormat = {

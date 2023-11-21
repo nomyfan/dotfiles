50abf245bf87aabf50721fe7c0dede23f7777329
nomyfan <Nomyfan@live.com>
2023-11-21 23:11:59 +0800

    .

--- nvim/lua/nvim-tree-telescope.lua
+++ nvim/lua/nvim-tree-telescope.lua
@@ -5,19 +5,6 @@ local actions = require'telescope.actions'
 local action_state = require'telescope.actions.state'
 local M = {}
 
-local view_selection = function(prompt_bufnr, map)
-  actions.select_default:replace(function()
-    actions.close(prompt_bufnr)
-    local selection = action_state.get_selected_entry()
-    local filename = selection.filename
-    if (filename == nil) then
-      filename = selection[1]
-    end
-    openfile.fn('preview', filename)
-  end)
-  return true
-end
-
 function M.launch_live_grep(opts)
   return M.launch_telescope("live_grep", opts)
 end
@@ -51,7 +38,6 @@ function M.launch_telescope(func_name, opts)
   opts = opts or {}
   opts.cwd = basedir
   opts.search_dirs = { basedir }
-  opts.attach_mappings = view_selection
   return require("telescope.builtin")[func_name](opts)
 end
 

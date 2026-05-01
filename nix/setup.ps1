f55c158577ba9a7592447e48938ad9061e1d44e2
nomyfan <Nomyfan@live.com>
2026-05-01 22:18:29 +0800

    .

--- nix/setup.ps1
+++ nix/setup.ps1
@@ -1,7 +1,9 @@
-Add-Content -Path /etc/nix/nix.conf -Value "substituters = https://mirrors.ustc.edu.cn/nix-channels/store https://cache.nixos.org"
+# Add-Content -Path /etc/nix/nix.conf -Value "substituters = https://mirrors.ustc.edu.cn/nix-channels/store https://cache.nixos.org"
+Add-Content -Path /etc/nix/nix.conf -Value "substituters = https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store https://cache.nixos.org"
 
 # Nix channels
-nix-channel --add https://mirrors.ustc.edu.cn/nix-channels/nixpkgs-unstable nixpkgs
+# nix-channel --add https://mirrors.ustc.edu.cn/nix-channels/nixpkgs-unstable nixpkgs
+nix-channel --add https://mirrors.tuna.tsinghua.edu.cn/nix-channels/nixpkgs-unstable nixpkgs
 nix-channel --update
 
 # Restart nix daemon
